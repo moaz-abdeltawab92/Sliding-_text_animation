61b41ff30a276b4e82c2c0a1f7b55749f9acb7c8
Moaz Ayman <113413632+moaz-abdeltawab92@users.noreply.github.com>
2025-04-12 17:21:34 +0200

    Add files via upload

---
+++ splash_view_body.dart
@@ -0,0 +1,50 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+import 'sliding_text.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 10), end: const Offset(0, 0))
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      //stretch main that child take all space of screen
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AppAssets.logo),
+        const SizedBox(
+          height: 5,
+        ),
+        SlidingText(slidingAnimation: slidingAnimation),
+      ],
+    );
+  }
+}
